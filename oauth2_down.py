a20a38d8c3fb2f0d27143d62c6cf27916b08e37c
ytakefuji <ytakefuji@gmail.com>
2019-01-16 14:36:01 +0900

    Add files via upload

---
+++ oauth2_down.py
@@ -0,0 +1,25 @@
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+import os,re
+gauth = GoogleAuth()
+gauth.LoadCredentialsFile("mycreds.txt")
+gauth.LocalWebserverAuth()
+drive = GoogleDrive(gauth)
+if gauth.credentials is None:
+    gauth.LocalWebserverAuth()
+elif gauth.access_token_expired:
+    gauth.Refresh()
+else:
+    gauth.Authorize()
+gauth.SaveCredentialsFile("mycreds.txt")
+file=drive.CreateFile()
+name=raw_input('file name? ')
+file['title']=name
+file_list = drive.ListFile({'q': "'root' in parents"}).GetList()
+for i in file_list:
+  m=re.search(name,i['title'])
+  if m:id=i['id']
+file['id']=id
+file.GetContentFile(name)
+os._exit(0)
+
